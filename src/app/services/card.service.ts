27840a2f67ffdcf2068907a7e52529b210bc79e5
Tanner Hart <tannerhart24@yahoo.com>
2022-06-29 16:13:37 -0700

    Added two mtg api urls to make requests to

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +7,7 @@ import {HttpClient} from "@angular/common/http";
 export class CardService {
 
   baseUrl: string = "https://api.scryfall.com";
+  dbUrl: string ='https://api.magicthegathering.io/v1/cards';
 
   constructor(private http: HttpClient) { }
 
@@ -14,4 +15,13 @@ export class CardService {
   getRandomCard() {
     return this.http.get(`${this.baseUrl}/cards/random`);
   }
+
+  getAllCards() {
+    return this.http.get(`${this.baseUrl}/`);
+  }
+
+  getAllCardsInDb() {
+    return this.http.get(`${this.dbUrl}`);
+  }
+
 }
