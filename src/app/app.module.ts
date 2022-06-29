2e9ab9a30f1a1b267f07e2462fb67ad111630198
Tanner Hart <tannerhart24@yahoo.com>
2022-06-29 16:15:50 -0700

    Included routes to each page and imported necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CardsComponent } from './cards/cards.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RandomComponent } from './pages/random/random.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './constants/footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'random', component: RandomComponent },
+  { path: 'all', component: CardsComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    HomeComponent,
+    RandomComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
