3f969281caf4b71308d40a60a206783c421ad8be
Tanner Hart <tannerhart24@yahoo.com>
2022-06-29 16:15:16 -0700

    Added components for three major features. Home page, all cards, random card, and footer.

--- src/app/pages/cards/cards.component.ts
+++ src/app/pages/cards/cards.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CardService} from "../services/card.service";
+import {CardService} from "../../services/card.service";
 
 @Component({
   selector: 'app-cards',
@@ -7,21 +7,27 @@ import {CardService} from "../services/card.service";
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
-
   cards: any = [];
+  allCards: any = [];
 
   constructor(private cardService: CardService) { }
 
   ngOnInit(): void {
-    this.getCardData();
+    this.getAllCards();
   }
-
 
   getCardData() {
     this.cardService.getRandomCard().subscribe(cardData => {
       this.cards = cardData;
-      console.log(cardData);
-    })
+    });
   }
+
+  getAllCards() {
+    this.cardService.getAllCardsInDb().subscribe(card => {
+      this.allCards = card;
+      console.log(card);
+    });
+  }
+
 
 }
---
+++ src/app/pages/random/random.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random',
+  templateUrl: './random.component.html',
+  styleUrls: ['./random.component.css']
+})
+export class RandomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
