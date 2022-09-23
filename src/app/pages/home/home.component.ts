0ac673285bfef20481bb060bc89d7deb856a060c
Tanner Hart <tannerhart24@yahoo.com>
2022-09-22 18:26:15 -0700

    Fetched all necessary cards for home page collage.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Card } from '../../models/card';
+import {CardService} from "../../services/card.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   isNew: boolean = false;
+  collageCards: Card[] = [];
+  cardOne: Card | null = null;
+  cardTwo: Card | null = null;
+  cardThree: Card | null = null;
+  cardFour: Card | null = null;
+  cardFive: Card | null = null;
+  cardSix: Card | null = null;
+  cardSeven: Card | null = null;
 
-  constructor() { }
+  constructor(private cardService: CardService) { }
 
   ngOnInit(): void {
+    this.getCollageCards();
   }
+
+  ngOnDestroy() {
+
+  }
+
+  getCollageCards() {
+    this.cardService.getTSS().subscribe((card) => this.cardOne = card);
+    this.cardService.getKarn().subscribe((card) => this.cardTwo = card);
+    this.cardService.getBTM().subscribe((card) => this.cardThree = card);
+    this.cardService.getYUP().subscribe((card) => this.cardFour = card);
+    this.cardService.getHF().subscribe((card) => this.cardFive = card);
+    this.cardService.getASR().subscribe((card) => this.cardSix = card);
+    this.cardService.getNDE().subscribe((card) => this.cardSeven = card);
+  }
+
 
 }
