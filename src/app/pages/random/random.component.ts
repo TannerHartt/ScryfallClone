741ec7fd2e5ed36985192f0b535acf0a0a8f20b1
Tanner Hart <tannerhart24@yahoo.com>
2022-10-11 14:57:14 -0700

    Created function to fetch a random card from api. Prevented memory leaks when the page is left.

--- src/app/pages/random/random.component.ts
+++ src/app/pages/random/random.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { CardService } from '../../services/card.service';
+import { Card } from '../../models/card';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-random',
   templateUrl: './random.component.html',
   styleUrls: ['./random.component.css']
 })
-export class RandomComponent implements OnInit {
+export class RandomComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  card: Card | null = null;
+  cardSubscription: Subscription;
+
+  constructor(private cardService: CardService) {
+    this.cardSubscription = new Subscription();
+  }
 
   ngOnInit(): void {
+    this.getRandomCard();
+  }
+
+  ngOnDestroy() {
+    this.cardSubscription.unsubscribe()
+  }
+
+  getRandomCard() {
+    this.cardSubscription = this.cardService.getRandomCard().subscribe((randomCard) => {
+      this.card = randomCard;
+    });
   }
 
 }
