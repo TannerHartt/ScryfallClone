09c70d69fbd08a3e00b7c0f4156b38701263cf6b
Tanner Hart <tannerhart24@yahoo.com>
2022-10-14 13:50:54 -0700

    Refactored the component to grab the query param "name" from the param map and to fetch the correct card to display.

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -1,23 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {CardService} from "../../services/card.service";
 import {Card} from "../../models/card";
 import {Subscription} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnDestroy {
 
-  constructor(private cardService: CardService) { }
+  constructor(private cardService: CardService, private route: ActivatedRoute) { }
 
   @Input() card: Card | null = null;
-  @Input() searchValue: Card | null = null;
+  @Input() searchValue: string = '';
+  subscription: Subscription = new Subscription();
 
+  // TODO create condition statement to handle which card to fetch
   ngOnInit(): void {
-
+    this.searchValue = this.route.snapshot.paramMap.get("name") as string;
+    this.getCard();
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.card = null;
+    this.searchValue = '';
+  }
+
+
+  getCard() {
+    this.subscription = this.cardService.getSearchValue(this.searchValue).subscribe((card) => {
+      this.card = card;
+    });
+  }
 
 }
