29193048785d877ac2641c7ff39fc16f0d2b0b23
Tanner Hart <tannerhart24@yahoo.com>
2022-10-14 20:43:49 -0700

    Created a model for type safety while working with set data.

---
+++ src/app/models/sets.ts
@@ -0,0 +1,21 @@
+export interface Sets {
+  object: string;
+  id: string;
+  code: string;
+  tcgplayer_id: number;
+  name: string;
+  uri: string;
+  scryfall_uri: string;
+  search_uri: string;
+  released_at: string;
+  set_type: string;
+  card_count: number;
+  printed_size: number;
+  parent_set_code: string;
+  digital: boolean;
+  nonfoil_only: boolean;
+  foil_only: boolean;
+  block_code: string;
+  block: string;
+  icon_svg_uri: string;
+}
