771e4d57aaf3a050b6df7fdd8e6e1497510089be
Tanner Hart <tannerhart24@yahoo.com>
2022-10-14 21:21:16 -0700

    Prevented memory leaks and implemented search functionality, correct page redirection, and search value formatting correction.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,26 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
+
+  searchValue: string = '';
 
   constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy() {
+    this.searchValue = '';
+  }
+
   checkPage() {
     if(this.router.url === '/card/random') {
       window.location.reload();
-      return;
     } else {
-      this.router.navigate(['/card/random']).then();
-      return;
+      this.router.navigate(['/card/random']).then(() => {
+        window.location.reload();
+      });
     }
   }
 
+  searchCard() {
+    if(this.searchValue) {
+      if (this.searchValue?.includes(' ')) {
+        this.searchValue = this.replaceAll(this.searchValue,' ','+');
+        this.router.navigate(['/card/' + this.searchValue]).then(() => {
+          window.location.reload();
+          this.searchValue = '';
+        });
+      } else {
+        this.router.navigate(['/card/' + this.searchValue]).then(() => {
+          window.location.reload();
+          this.searchValue = '';
+        });
+      }
+    }
+  }
+
+  public replaceAll(str: string, find: string, replace: string) {
+    return str.replace(new RegExp(find,'g'), replace);
+  }
 }
