e0a38323d090f8b8d4457417bd114ae5aa03b348
Tanner Hart <tannerhart24@yahoo.com>
2022-10-15 17:13:42 -0700

    Removed all unused code and imports from this component after being refactored. It only displays components now.

--- src/app/pages/random/random.component.ts
+++ src/app/pages/random/random.component.ts
@@ -1,36 +1,19 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { CardService } from '../../services/card.service';
-import { Card } from '../../models/card';
-import { Subscription } from 'rxjs';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-random',
   templateUrl: './random.component.html',
   styleUrls: ['./random.component.css']
 })
-export class RandomComponent implements OnInit, OnDestroy {
+export class RandomComponent implements OnInit {
 
-  card: Card | null = null;
-  cardSubscription: Subscription;
-
-  constructor(private cardService: CardService) {
-    this.cardSubscription = new Subscription();
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getRandomCard();
   }
 
-  ngOnDestroy() {
-    this.cardSubscription.unsubscribe();
-    this.card = null;
-  }
 
-  getRandomCard() {
-    this.cardSubscription = this.cardService.getRandomCard().subscribe((randomCard) => {
-      this.card = randomCard;
-    });
-  }
+
 
 }
