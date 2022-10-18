707e5b7fdbf80126c301c2f66a2abed33041e19b
Tanner Hart <tannerhart24@yahoo.com>
2022-10-17 23:09:44 -0700

    Created, styled, and filled the list display page. Further implementation to be done.

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CardService} from "../../services/card.service";
+import {Card} from "../../models/card";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  cards: Card[] = [];
+
+  constructor(private cardService: CardService, private router: Router) { }
+
 
   ngOnInit(): void {
+    // this.getRedWhites();
+    this.getChandra();
+  }
+
+  redirect(card: Card) {
+    this.router.navigate(['/card/' + card.name]).then(() => {
+      window.location.reload();
+    });
+  }
+
+  getChandra() {
+    this.cardService.getChandra().subscribe((card) => {
+      this.cards = card;
+    })
+  }
+
+
+  getRedWhites() {
+    this.cardService.getRedWhiteOneDrops().subscribe((card) => {
+      this.cards = card;
+      console.log(card);
+    });
   }
 
 }
