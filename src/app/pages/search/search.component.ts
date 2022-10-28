d72c337ea7d8f1323189c4ca68ebda5df714e0a5
Tanner Hart <tannerhart24@yahoo.com>
2022-10-28 15:05:50 -0700

    Removed temporary development functions and replaced the with a function that correctly grabs the search value query param and fetches it from the API to store and manage. Added scroll to top functionality on redirection.

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,43 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import {CardService} from "../../services/card.service";
-import {Card} from "../../models/card";
-import {Router} from "@angular/router";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CardService } from '../../services/card.service';
+import { Card } from '../../models/card';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
 
   cards: Card[] = [];
+  searchValue: string = '';
+  subscription: Subscription = new Subscription();
 
-  constructor(private cardService: CardService, private router: Router) { }
+  constructor(private cardService: CardService, private route: ActivatedRoute, private router: Router) { }
 
 
   ngOnInit(): void {
-    // this.getRedWhites();
-    this.getChandra();
+    this.searchValue = this.route.snapshot.paramMap.get('searchValue') as string;
+    this.getSearchCards();
   }
 
+  // Resetting all variables and subscriptions to avoid memory leaks.
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.cards = [];
+    this.searchValue = '';
+  }
+
+  // This function fetches and stores all cards that contain the user entered search value.
+  getSearchCards() {
+    this.subscription = this.cardService.getSearchCards(this.searchValue).subscribe((card) => {
+      this.cards = card;
+    });
+  }
+
+  // This function redirects the user when they click on one of the card display elements.
   redirect(card: Card) {
     this.router.navigate(['/card/' + card.name]).then(() => {
       window.location.reload();
+      window.scrollTo(0, 0);
     });
   }
 
-  getChandra() {
-    this.cardService.getChandra().subscribe((card) => {
-      this.cards = card;
-    })
-  }
-
-
-  getRedWhites() {
-    this.cardService.getRedWhiteOneDrops().subscribe((card) => {
-      this.cards = card;
-      console.log(card);
-    });
-  }
 
 }
