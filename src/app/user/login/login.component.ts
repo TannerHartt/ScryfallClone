7621475797eecd710e2f225213d9dcd358ed859b
Tanner Hart <tannerhart24@yahoo.com>
2022-11-22 11:31:56 -0800

    Created a new page component to handle user login and registration.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
 
+  // TODO continue login work and login page template.
+  // TODO create initialize firebase into project and make connection.
   ngOnInit(): void {
   }
 
