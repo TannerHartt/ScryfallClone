4ce5f0611c47a3dba05019a6f72c0527a30ef1c8
Tanner Hart <tannerhart24@yahoo.com>
2022-11-29 17:44:32 -0800

    Created initial variables and pre-made logout function.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private userCollection: AngularFirestoreCollection<User>;
+
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
+    this.userCollection = db.collection('users');
+  }
+
+
+
+  public async logout($event?: Event) {
+    if($event) { // If an event happens, prevent the default browser behavior of refreshing the page.
+      $event.preventDefault();
+    }
+    await this.auth.signOut(); // Call the firebase given sign out method
+
+  }
 }
