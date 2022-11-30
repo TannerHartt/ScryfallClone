dca6a7cafcdea871dc980f05252e551160528493
Tanner Hart <tannerhart24@yahoo.com>
2022-11-30 15:20:35 -0800

    Minor cleanups.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -10,8 +10,6 @@ export class LoginComponent implements OnInit {
 
   constructor(private auth: AngularFireAuth) { }
 
-  // TODO continue login work and login page template.
-  // TODO create initialize firebase into project and make connection.
   ngOnInit(): void {
   }
 
