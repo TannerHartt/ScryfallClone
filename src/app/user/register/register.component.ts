13404de94be0ba8f23f47930250d8181037ebba0
Tanner Hart <tannerhart24@yahoo.com>
2022-11-30 15:20:11 -0800

    Created a reactive form group and managed each control with validation and registration. Cleaned up unused code.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css', '../login/login.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  email: FormControl = new FormControl<String>('', [Validators.email, Validators.required, Validators.minLength(6)]);
+  password: FormControl = new FormControl<String>('', [Validators.required, Validators.minLength(5), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
+  username: FormControl = new FormControl<String>('', [Validators.required, Validators.minLength(5)]);
+
+  registerForm = new FormGroup({
+    email: this.email,
+    password: this.password,
+    username: this.username
+  });
+
+
+  async register() {
+    try {
+      // Calls the firebase provided create user function and passes in the values entered by the user in the register form.
+      await this.auth.createUser(this.registerForm.value as User)
+        .then(() => {
+          setTimeout(() => {
+            this.router.navigate(['/'])
+              .then(() => {
+                window.location.reload();
+              });
+          }, 3000);
+        });
+    } catch (e) { // If an error occurs, display relevant info to the user indicating an error occurred.
+      console.log(e);
+      return; // End function compilation and return.
+    }
   }
 
 }
