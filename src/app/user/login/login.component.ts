07e96a195b344b8b00d1d47b7cf37b986bd23295
Tanner Hart <tannerhart24@yahoo.com>
2022-12-01 23:14:26 -0800

    Included a redirect on successful login and an alert error on failed login attempt.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor(private auth: AngularFireAuth) { }
+  constructor(private auth: AngularFireAuth, private router: Router) { }
 
   credentials = {
     email: '',
     password: '',
   }
-
-  ngOnInit(): void {
-  }
-
 
   async login() {
     try {
@@ -25,8 +22,17 @@ export class LoginComponent implements OnInit {
       // Calls the firebase provided sign in method and passes in the user credentials to check if they are registered.
       await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
     } catch (e) { // If an error occurs... ==>
+      alert('Login Error, please try again.');
       return // Ends the login attempt and returns from the function.
     }
+
+    setTimeout(() => {
+      this.router.navigate(['/settings']).then(() => {
+        window.location.reload();
+      });
+    }, 1000);
+
+
 
   }
 }
