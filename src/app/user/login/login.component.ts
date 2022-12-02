2f951215f904056701e6ed55a0d29cc2d61a55e3
Tanner Hart <tannerhart24@yahoo.com>
2022-12-01 16:33:39 -0800

    Created an async function to handle login functionality. Variable to store the entry.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -10,7 +10,23 @@ export class LoginComponent implements OnInit {
 
   constructor(private auth: AngularFireAuth) { }
 
+  credentials = {
+    email: '',
+    password: '',
+  }
+
   ngOnInit(): void {
   }
 
+
+  async login() {
+    try {
+      console.log(this.credentials)
+      // Calls the firebase provided sign in method and passes in the user credentials to check if they are registered.
+      await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
+    } catch (e) { // If an error occurs... ==>
+      return // Ends the login attempt and returns from the function.
+    }
+
+  }
 }
