b3585990e5d78fd595512d7963acabcc838e09d2
Tanner Hart <tannerhart24@yahoo.com>
2022-12-05 21:03:38 -0800

    Restructured file locations slightly.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,11 @@ import { AuthService } from '../../services/auth.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, public router: Router) { }
 
   ngOnInit(): void {
   }
+
 
   async logout($event: Event) {
     await this.auth.logout($event);
