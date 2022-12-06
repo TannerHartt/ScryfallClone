eab74cf813674fb11a8c5c83e015ac1571b7a5d2
Tanner Hart <tannerhart24@yahoo.com>
2022-12-05 21:02:21 -0800

    Created child components to each handle a specific profile setting.

---
+++ src/app/dashboard/settings/email-security/email-security.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-security',
+  templateUrl: './email-security.component.html',
+  styleUrls: ['./email-security.component.css']
+})
+export class EmailSecurityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
